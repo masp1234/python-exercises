c4555dd38ad37d206a4ea024da6be97993bad58c
masp1234 <mart2c04@stud.kea.dk>
2023-03-11 17:30:50 +0100

    thehub-scraper scraping links

--- 05_Utilities_and_Modules/5.5_webscraper/thehub-webscraper.py
+++ 05_Utilities_and_Modules/5.5_webscraper/thehub-webscraper.py
@@ -8,10 +8,27 @@ session = HTMLSession()
 links = []
 
 def scrape_links():
+    more_content = True
     page_number = 0
-    response = session.get(f'{url}&page={page_number}')
-    print(response)
+    links = []
+    while more_content is True:
+        response = session.get(f'{url}&page={page_number}')
+        response.html.render(sleep=1)
+        search_words = ['python']
+        job_results = response.html.find('.card__content')
+        if len(job_results) <= 0:
+            print("no results")
+        else:
+            for job_result in job_results:
+                print(job_result)
+                links.append(get_job_result_link(job_result))
+                print(len(job_results))
+                page_number = page_number + 1
+                print(page_number)
 
 
+def get_job_result_link(job_result):
+    link = job_result.find('.card-job-find-list__link', first=True).attrs['href']
+    print(link)
 
 scrape_links()
